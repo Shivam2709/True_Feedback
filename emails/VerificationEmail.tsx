5c2b926dff24c821b4862c14148c9c6739699a66
Shivam Thakur <shivamthakur2709@gmail.com>
2024-11-15 18:00:17 +0530

    email verification

---
+++ emails/VerificationEmail.tsx
@@ -0,0 +1,58 @@
+import {
+  Html,
+  Head,
+  Preview,
+  Text,
+  Font,
+  Heading,
+  Row,
+  Section,
+} from "@react-email/components";
+
+interface VerificationEmailProps {
+  username: string;
+  otp: string;
+}
+
+export default function VerificationEmail({
+  username,
+  otp,
+}: VerificationEmailProps) {
+  return (
+    <Html>
+      <Head>
+        <title>Verification Code</title>
+        <Font
+          fontFamily="Roboto"
+          fallbackFontFamily="Verdana"
+          webFont={{
+            url: "https://fonts.gstatic.com/s/roboto/v30/KFOmCnqEu92Fr1Me5Q.ttf",
+            format: "woff2",
+          }}
+          fontWeight={400}
+          fontStyle="normal"
+        />
+      </Head>
+      <Preview>Here&apos;s your Verification code: {otp}</Preview>
+      <Section>
+        <Row>
+          <Heading as="h2">Hello {username}</Heading>
+        </Row>
+        <Row>
+          <Text>
+            Thank you for registering. Please use the following verification
+            code to complete your registration:
+          </Text>
+        </Row>
+        <Row>
+          <Text>{otp}</Text>
+        </Row>
+        <Row>
+          <Text>
+            If you did not request this code, please ignore this email.
+          </Text>
+        </Row>
+      </Section>
+    </Html>
+  );
+}
